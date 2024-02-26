b99309c36525e00ec0be62ea193c4287b3fff2fb
小西巴 <c3p0ooo@xiba.com>
2024-02-26 15:40:58 +0800

    修改目录穿越、文件上传

--- src/main/java/com/best/hello/controller/Traversal.java
+++ src/main/java/com/best/hello/controller/Traversal.java
@@ -6,11 +6,11 @@ import io.swagger.annotations.ApiOperation;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.file.Files;
@@ -18,6 +18,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 /**
  * @date 2021/07/15
@@ -25,7 +28,7 @@ import java.util.List;
 @Api("目录遍历")
 @RestController
 @RequestMapping("/Traversal")
-public class Traversal {
+public class Traversal extends HttpServlet {
     Logger log = LoggerFactory.getLogger(Traversal.class);
 
     @ApiOperation(value = "vul：任意文件下载")
@@ -75,6 +78,38 @@ public class Traversal {
         }
     }
 
+    @ApiOperation(value = "vul：解压文件")
+    @PostMapping("/unzip")
+    public void unzip(@RequestParam("file") MultipartFile zipfile, HttpServletResponse response) throws Exception {
+        // 获取文件名
+        String fileName = zipfile.getOriginalFilename();
+        // 获取文件后缀
+        String prefix = fileName.substring(fileName.lastIndexOf("."));
+        File file = File.createTempFile(fileName, prefix);
+        zipfile.transferTo(file);
+
+        ZipFile zf = new ZipFile(file);
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(file));
+        ZipEntry entry;
+        ServletOutputStream outputStream = response.getOutputStream();
+        //遍历zip中的文件
+        while((entry = zis.getNextEntry()) != null){
+            //构造输出流
+            String zipfilename = entry.getName();
+            File outfile = new File(System.getProperty("user.dir") + "/src/main/resources/static/file/" + zipfilename);
+            FileOutputStream fos = new FileOutputStream(outfile);
+            //将zip中的内容读出
+            InputStream inputStream = zf.getInputStream(entry);
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            //写文件并关闭连接
+            outputStream.write(("解压路径：" + outfile.getPath()).getBytes("gbk"));
+            fos.write(bytes);
+            fos.close();
+            inputStream.close();
+        }
+    }
+
 
     /*@ApiOperation(value = "safe：过滤../")
     @GetMapping("/download/safe")
@@ -94,6 +129,42 @@ public class Traversal {
             return "安全路径：" + filePath;
         } else {
             return "检测到非法遍历！";
+        }
+    }
+
+    @PostMapping("/unzip/safe")
+    public void unzipSafe(@RequestParam("file") MultipartFile zipfile, HttpServletResponse response) throws Exception {
+        // 获取文件名
+        String fileName = zipfile.getOriginalFilename();
+        // 获取文件后缀
+        String prefix = fileName.substring(fileName.lastIndexOf("."));
+        File file = File.createTempFile(fileName, prefix);
+        zipfile.transferTo(file);
+
+        ZipFile zf = new ZipFile(file);
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(file));
+        ZipEntry entry;
+        ServletOutputStream outputStream = response.getOutputStream();
+        //遍历zip中的文件
+        while((entry = zis.getNextEntry()) != null){
+            //构造输出流
+            String zipfilename = entry.getName();
+            String filepath = System.getProperty("user.dir") + "/src/main/resources/static/file/" + zipfilename;
+            if (!checkFile(filepath) || !checkExt(filepath)){
+                outputStream.write("解压失败，zip文件存在问题！".getBytes("GBK"));
+                return;
+            }
+            File outfile = new File(filepath);
+            FileOutputStream fos = new FileOutputStream(outfile);
+            //将zip中的内容读出
+            InputStream inputStream = zf.getInputStream(entry);
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            //写文件并关闭连接
+            outputStream.write(("解压路径：" + outfile.getPath()).getBytes("gbk"));
+            fos.write(bytes);
+            fos.close();
+            inputStream.close();
         }
     }
     public boolean checkFile(String path) throws Exception {
