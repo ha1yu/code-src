659e9f25f2e06d8ee06bb21fecb2fb4301edd12a
Mechoy <mechoy@yeah.net>
2024-02-28 16:16:12 +0800

    update tkMapper

--- src/main/java/com/best/hello/controller/SQLI/tkMapper.java
+++ src/main/java/com/best/hello/controller/SQLI/tkMapper.java
@@ -1,4 +1,13 @@
 package com.best.hello.controller.SQLI;
 
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api("SQL注入 - tkMapper")
+@Slf4j
+@RestController
+@RequestMapping("/SQLI/tkMapper")
 public class tkMapper {
 }
